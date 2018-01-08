74e3464b5f10d642e4ce84f0567619406da86c4c
Eduardo Bianchi <bianchieduardo@gmail.com>
2018-01-08 09:10:08 -0300

    Criado repository e service para Categoria

---
+++ src/main/java/br/com/bpsistemas/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package br.com.bpsistemas.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.bpsistemas.cursomc.domain.Categoria;
+import br.com.bpsistemas.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		 Categoria obj = repo.findOne(id);
+		 return obj;
+	}
+	
+}

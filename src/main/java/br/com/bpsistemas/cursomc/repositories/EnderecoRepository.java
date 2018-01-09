7f380c0c650e9e1e0c892f11f80b5cc8819b15b1
Eduardo Bianchi <bianchieduardo@gmail.com>
2018-01-09 08:52:38 -0300

    Cliente, TipoCliente, Telefones e enderecos

---
+++ src/main/java/br/com/bpsistemas/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package br.com.bpsistemas.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.bpsistemas.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+	
+}
